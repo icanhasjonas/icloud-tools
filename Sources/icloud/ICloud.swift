690e50265552d841d20e140163d1ed6ce41676b2
Jonas Frost <j@duplo.org>
2026-04-19 20:14:48 +0700

    Add --version flag (0.3.1)

--- Sources/icloud/ICloud.swift
+++ Sources/icloud/ICloud.swift
@@ -5,6 +5,7 @@ struct ICloud: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "icloud",
         abstract: "Manage iCloud Drive files from the command line.",
+        version: "0.3.1",
         subcommands: [StatusCommand.self, MoveCommand.self, CopyCommand.self],
         defaultSubcommand: StatusCommand.self
     )
