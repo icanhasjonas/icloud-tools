d047247ef258b7f8da457dfcfc5cdcfe8294d34c
Jonas Frost <j@duplo.org>
2026-04-19 20:34:45 +0700

    Single source of truth for version (Version.swift)

--- Sources/icloud/ICloud.swift
+++ Sources/icloud/ICloud.swift
@@ -6,7 +6,7 @@ struct ICloud: ParsableCommand {
         commandName: "icloud",
         abstract: "Manage iCloud Drive files.",
         discussion: "Replacement for brctl download/evict (removed in macOS 14+).",
-        version: "0.4.0",
+        version: version,
         subcommands: [StatusCommand.self, DownloadCommand.self, EvictCommand.self, MoveCommand.self, CopyCommand.self],
         defaultSubcommand: StatusCommand.self
     )
